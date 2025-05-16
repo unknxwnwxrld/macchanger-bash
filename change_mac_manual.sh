bcd4ea2ad0ac90caad53e2643a9b0ef500bf167b
unknxwnwxrld <smerechinskiy.oleksii@gmail.com>
2025-05-16 09:25:08 +0200

    added main script

---
+++ change_mac_manual.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+NEW_MAC="10:7c:61:65:5d:a4"
+IFACE="enp4s0"
+
+echo "[*] Назначаю $NEW_MAC на $IFACE"
+
+sudo ip link set $IFACE down
+sudo ip link set $IFACE address $NEW_MAC
+sudo ip link set $IFACE up
+
+sudo dhclient -r $IFACE
+sudo dhclient -1 $IFACE &
+
+DHCLIENT_PID=$!
+
+sleep 2
+
+if kill -0 $DHCLIENT_PID 2>/dev/null; then
+	echo "[*] dhclient не завершился, автоматичекое прерывание..."
+	sudo kill $DHCLIENT_PID
+fi
+
+ip a show $IFACE | grep inet
